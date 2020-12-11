133c212c0704be99a916e4fa02a88bde928d8c82
craus90 <craus902@gmail.com>
2020-12-11 11:58:08 +0400

    move by axis wip

---
+++ Assets/Scripts/XR/MoveByAxes.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.XR;
+
+//public class MoveByAxe : MonoBehaviour
+//{
+//    public string xAxis;
+//    public string yAxis;
+
+//    public Transform target;
+//    public Transform relativeTo;
+
+//    public Vector3 direction;
+
+//    private Vector3 absoluteDirection => relativeTo != null ? relativeTo.TransformDirection(direction) : direction;
+
+//    public bool ignoreVerticalMovement = false;
+
+//    public float speed = 1;
+
+//    public float value => Input.GetAxisRaw(axe);
+
+//    private void Awake() {
+//        if (target == null) {
+//            target = transform;
+//        }
+//    }
+
+//    void Update() {
+//        if (relativeTo == null) {
+//            target.Translate(direction * value * speed * Time.deltaTime, Space.World);
+//        } else {
+//            target.Translate( * value * speed * Time.deltaTime, Space.World);
+//        }
+//    }
+//}
--- Assets/Scripts/XR/XRNodeFollow.cs
+++ Assets/Scripts/XR/XRNodeFollow.cs
@@ -7,8 +7,15 @@ public class XRNodeFollow : MonoBehaviour
 {
     public XRNode node;
 
+    public bool local = true;
+
     void Update() {
-        transform.position = XRInput.node(node).Position() ?? transform.position;
-        transform.rotation = XRInput.node(node).Rotation() ?? transform.rotation;
+        if (local) {
+            transform.localPosition = XRInput.node(node).Position() ?? transform.localPosition;
+            transform.localRotation = XRInput.node(node).Rotation() ?? transform.localRotation;
+        } else {
+            transform.position = XRInput.node(node).Position() ?? transform.position;
+            transform.rotation = XRInput.node(node).Rotation() ?? transform.rotation;
+        }
     }
 }
